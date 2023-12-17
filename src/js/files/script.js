e42b3b27260b9cc637ea707fd076d392f88a7d63
Corkerro <osenny.roman@gmail.com>
2023-12-17 16:39:09 +0200

    Add hero section (fix)

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,9 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var video = document.getElementById('heroVideo');
+  video.play();
+});
